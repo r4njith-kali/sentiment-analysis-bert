2bdb71dd859c5dd6c4508e12624d64b49233b14f
r4njith-kali <e0958148@u.nus.edu>
2025-04-05 21:23:49 +0800

    Importing data into train.py

--- src/train.py
+++ src/train.py
@@ -1,2 +1,34 @@
 # src/train.py
 
+import os
+import numpy as np
+import evaluate 
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    TrainingArguments, 
+    Trainer,
+    set_seed
+)
+import config
+from src.data_loader import load_and_preprocess_data
+import torch
+
+print(f"Setting random seed to: {config.SEED}")
+set_seed(config.SEED)
+
+print(f"Loading tokenizer: {config.MODEL_NAME}")
+tokenizer = AutoTokenizer.from_pretrained(config.MODEL_NAME)
+
+
+print("Loading and preprocessing data...")
+
+tokenized_datasets = load_and_preprocess_data(
+    tokenizer_name = config.MODEL_NAME,
+    dataset_name = config.DATASET_NAME,
+    text_column = config.TEXT_COLUMN,
+    label_column = config.LABEL_COLUMN,
+    max_length = config.MAX_LENGTH,
+    seed = config.SEED
+)
+
