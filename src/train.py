7d5b08b2996b2a801d79ef08e44615911b8c57e3
r4njith-kali <e0958148@u.nus.edu>
2025-04-05 21:40:40 +0800

    Loading model with datasets

--- src/train.py
+++ src/train.py
@@ -18,7 +18,7 @@ print(f"Setting random seed to: {config.SEED}")
 set_seed(config.SEED)
 
 print(f"Loading tokenizer: {config.MODEL_NAME}")
-tokenizer = AutoTokenizer.from_pretrained(config.MODEL_NAME)
+tokenizer = AutoTokenizer.from_pretrained(config.MODEL_NAME) # Not necessary as it as already loaded in load_and_preprocess_data() func but good practice
 
 
 print("Loading and preprocessing data...")
@@ -31,4 +31,13 @@ tokenized_datasets = load_and_preprocess_data(
     max_length = config.MAX_LENGTH,
     seed = config.SEED
 )
+
+train_dataset = tokenized_datasets["train"]
+eval_dataset = tokenized_datasets["test"]
+
+print(f"Number of training examples: {len(train_dataset)}")
+print(f"Number of evaluation examples: {len(eval_dataset)}")
+
+# ------ LOAD MODEL --------
+
 
