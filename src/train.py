ff61317139c66e8fb980a05fe51d2eeb2269189b
r4njith-kali <e0958148@u.nus.edu>
2025-04-05 17:58:02 +0000

    Feat: Update scripts and complete training in Colab

--- src/train.py
+++ src/train.py
@@ -61,7 +61,7 @@ metric = evaluate.load("accuracy")
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis = -1)
-    return metric.compute(predictions, references = labels)
+    return metric.compute(predictions=predictions, references = labels)
 
 # -----Define Training Arguments-------
 
