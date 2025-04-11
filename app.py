3ed506243e10fcf19f9d3d64cc4ebd0a1d8a2430
r4njith-kali <e0958148@u.nus.edu>
2025-04-11 20:08:24 +0800

    Added multiple review feature

--- app.py
+++ app.py
@@ -12,23 +12,30 @@ def load_model():
 
 tokenizer, model = load_model()
 
-#st.set_page_config(page_title = "Sentiment Analyser", layout = "centered")
 st.title("Sentiment Analyzer using BERT")
+st.markdown("Enter the number of reviews you want to analyze:")
+
+num_reviews = st.number_input("Number of reviews", min_value = 1, max_value = 10, value = 1, step = 1)
+
+
 st.markdown("Enter a review or a comment and get its Sentiment Analyzed.")
 
-user_input = st.text_area("Enter your text here:", height = 200)
+user_inputs = []
+
+for i in range(num_reviews):
+    user_input = st.text_area(f"Review {i+1}", height = 100, key = f"review_{i}")
+    user_inputs.append(user_input)
 
 if st.button("Analyze Sentiment"):
-    if not user_input.strip():
-        st.warning("Please enter some text to analyse")
-    elif tokenizer is None or model is None:
-        st.error("Model is not loaded. Please check logs.")
-    else:
-        with st.spinner("Analysing..."):
-            prediction, probs = predict_sentiment(user_input,tokenizer, model)
-            st.success(f'Prediction: {prediction}')
-
-            st.subheader("Confidence scores")
-            st.bar_chart(probs)
-
+    for idx, review in enumerate(user_inputs):
+        if not review.strip():
+            st.warning(f"Review {idx+1} is empty. Please enter text.")
+        elif tokenizer is None or model is None:
+            st.error("Model is not loaded. Please check logs")
+        else:
+            with st.spinner(f"Analyzing review {idx+1}..."):
+                prediction, probs = predict_sentiment(review, tokenizer, model)
+                st.subheader(f"Review {idx+1}:")
+                st.success(f'Prediction: {prediction}')
+                st.bar_chart(probs)
 
